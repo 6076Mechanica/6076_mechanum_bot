1b9e2a3ee2bc07a429a1863019a33f45dca358e7
Gideon <gideoncarleton@gmail.com>
2025-02-27 01:50:59 -0800

    added consants for the claw, elevator and others

--- src/main/java/frc/robot/constants.java
+++ src/main/java/frc/robot/constants.java
@@ -2,7 +2,16 @@ package frc.robot;
 
 public class constants {
     public static final int driverStick = 0;
-    public static final int operatorStick = 1;
+    public static final double elRetractionSpeed = 0.5; // Originally 0.5
+    public static final double elExtensionSpeed = 1; // Originally 1
+
+    public static final double operatorDeadband = 0.0; // Originally 0.0
+
+    public static final double wristSteadySpeed = 0.15; // Originally 0.15
+    public static final double wristRegSpeed = 0.55; // Originally 0.55
+    public static final double clawSpeed = 0.5; // Originally 0.5
+    public static final double clawSpeedReverse = -0.5; // Originally -0.5
+
 
     
 }
